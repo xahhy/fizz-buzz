abcc0d7e72f4747c3ea956d35f1196bc60dc5982
hoyhe <hoyhe@thoughtworks.com>
2020-01-10 21:35:06 +0800

    feature: return Fizz when number can be multiplied by 3

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,5 +1,8 @@
 public class FizzBuzz {
     static String fizzBuzz(int number){
+        if(number % 3 == 0){
+            return "Fizz";
+        }
         return String.valueOf(number);
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -6,8 +6,15 @@ public class FizzBuzzTest {
 
     @Test
     public void should_return_number() {
-        int number = 0;
+        int number = 1;
         String result = FizzBuzz.fizzBuzz(number);
-        assertEquals("0", result);
+        assertEquals("1", result);
+    }
+
+    @Test
+    public void should_return_fizz_when_3() {
+        int number = 3;
+        String result = FizzBuzz.fizzBuzz(number);
+        assertEquals("Fizz", result);
     }
 }
