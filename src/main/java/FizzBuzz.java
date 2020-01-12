facb22b7f7aa852a8d117ab2960f0bb328a5cf38
hoyhe <hoyhe@thoughtworks.com>
2020-01-12 20:30:58 +0800

    feature: return Fizz/Buzz/Whizz based on number can be multiplied by 3/5/7

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,5 +1,14 @@
 public class FizzBuzz {
     static String fizzBuzz(int number){
+        if(number % 3 == 0){
+            return "Fizz";
+        }else if(number % 5 == 0){
+            return "Buzz";
+        }
+        else if(number % 7 == 0){
+            return "Whizz";
+        }
         return String.valueOf(number);
     }
+
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -6,8 +6,37 @@ public class FizzBuzzTest {
 
     @Test
     public void should_return_number() {
-        int number = 0;
+        int number = 1;
         String result = FizzBuzz.fizzBuzz(number);
-        assertEquals("0", result);
+        assertEquals("1", result);
     }
+
+    @Test
+    public void should_return_fizz_when_number_can_multiplied_by_3() {
+        String result = FizzBuzz.fizzBuzz(3);
+        assertEquals("Fizz", result);
+
+        String result2 = FizzBuzz.fizzBuzz(6);
+        assertEquals("Fizz", result2);
+
+    }
+
+    @Test
+    public void should_return_buzz_when_number_can_multiplied_by_5() {
+        String result = FizzBuzz.fizzBuzz(5);
+        assertEquals("Buzz", result);
+
+        String result2 = FizzBuzz.fizzBuzz(10);
+        assertEquals("Buzz", result2);
+    }
+
+    @Test
+    public void should_return_whizz_when_number_can_multiplied_by_7() {
+        String result = FizzBuzz.fizzBuzz(7);
+        assertEquals("Whizz", result);
+
+        String result2 = FizzBuzz.fizzBuzz(14);
+        assertEquals("Whizz", result2);
+    }
+
 }
